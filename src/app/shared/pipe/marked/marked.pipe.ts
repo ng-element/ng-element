ee912c75ebc415c601aa47c9d78ce65e384cfe8e
llkui241569 <llkui241569@163.com>
2025-01-03 22:04:15 +0800

    Updated to use theme-chalk

--- src/app/shared/pipe/marked/marked.pipe.ts
+++ src/app/shared/pipe/marked/marked.pipe.ts
@@ -1,9 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import marked from 'marked';
+import { marked } from 'marked';
 
 @Pipe({
-    name: 'marked',
-    standalone: false
+  name: 'marked',
+  standalone: false
 })
 export class MarkedPipe implements PipeTransform {
   transform(value: any): any {
